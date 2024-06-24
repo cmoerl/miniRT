090f30bce2b0fa3dfa66d1468c5a33e056459ccd
Carla Juliana Sturm <csturm@c1r1p2.42vienna.com>
2024-06-24 17:50:55 +0200

    trying to understand getting the ray

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:03:05 by csturm            #+#    #+#             */
-/*   Updated: 2024/06/21 13:16:51 by csturm           ###   ########.fr       */
+/*   Updated: 2024/06/24 17:50:21 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,26 @@
 - show the image 
 */
 
+t_vector    get_ray(t_scene scene, int x, int y)
+{
+    t_vector ray;
+    t_vector screen_point;
+
+// normalising the pixel coordinates to the range [0, 1]
+// remapping the pixel coordinates to the range [-1, 1]
+    screen_point.x = (2 * (x + 0.5) / (double)scene.width - 1) * scene.aspect_ratio * scene.tan_fov;
+    screen_point.y = (1 - 2 * (y + 0.5) / (double)scene.height) * scene.tan_fov;
+    screen_point.z = -1;
+    ray = normalize_vector(screen_point);
+    return (ray);
+}
+
 void    render_scene(t_scene scene)
 {
     int x;
     int y;
+    t_vector ray;
+    t_color color;
 
     x = 0;
     y = 0;
@@ -33,9 +49,9 @@ void    render_scene(t_scene scene)
     {
         while (x < scene.width)
         {
-            // get ray
-            // trace ray
-            // put color on screen
+            ray = get_ray(scene, x, y);
+            color = trace_ray(scene, ray);
+            put_pixel(x, y, color);
             x++;
         }
         y++;
