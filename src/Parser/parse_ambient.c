843535288d86993ca7af38a3064a266d5e3caa81
MarSonyTM <56359142+MarSonyTM@users.noreply.github.com>
2024-08-19 14:00:35 +0200

    ambnient at 29 lines

--- src/Parser/parse_ambient.c
+++ src/Parser/parse_ambient.c
@@ -6,7 +6,7 @@
 /*   By: marianfurnica <marianfurnica@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 08:41:08 by marianfurni       #+#    #+#             */
-/*   Updated: 2024/08/19 13:58:03 by marianfurni      ###   ########.fr       */
+/*   Updated: 2024/08/19 14:00:04 by marianfurni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,35 +48,33 @@ void	parse_color_value(char *line, int *i,
 
 void	parse_ambient(char *line, t_amblight *ambient)
 {
-    int i;
-    float intensity;
-    int r, g, b;
+	int		i;
+	int		r;
+	int		g;
+	int		b;
+	float	intensity;
 
-    i = 0;
-    skip_whitespace(line, &i);
-    if (line[i] != 'A')
-        error("Missing 'A' identifier for ambient lighting", NULL);
-    i++;
-    skip_whitespace(line, &i);
-    parse_intensity(line, &i, &intensity);
-    skip_whitespace(line, &i);
-    parse_color_value(line, &i, &r, "red");
-    while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == ','))
-        i++;
-    parse_color_value(line, &i, &g, "green");
-    while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == ','))
-        i++;
-    parse_color_value(line, &i, &b, "blue");
-    while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == '\n'))
-        i++;
-    if (line[i] != '\0')
-        error("Invalid character in ambient lighting definition", NULL);
-    ambient->intensity = intensity;
-    ambient->color.r = r / 255.0;
-    ambient->color.g = g / 255.0;
-    ambient->color.b = b / 255.0;
-    printf("Parsed Ambient Lighting:\n");
-    printf("  Intensity: %f\n", intensity);
-    printf("  Color: R=%d, G=%d, B=%d\n", r, g, b);
-    printf("  Normalized Color: R=%f, G=%f, B=%f\n", ambient->color.r, ambient->color.g, ambient->color.b);
+	i = 0;
+	skip_whitespace(line, &i);
+	if (line[i] != 'A')
+		error("Missing 'A' identifier for ambient lighting", NULL);
+	i++;
+	skip_whitespace(line, &i);
+	parse_intensity(line, &i, &intensity);
+	skip_whitespace(line, &i);
+	parse_color_value(line, &i, &r, "red");
+	while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == ','))
+		i++;
+	parse_color_value(line, &i, &g, "green");
+	while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == ','))
+		i++;
+	parse_color_value(line, &i, &b, "blue");
+	while (line[i] && (line[i] == ' ' || line[i] == '\t' || line[i] == '\n'))
+		i++;
+	if (line[i] != '\0')
+		error("Invalid character in ambient lighting definition", NULL);
+	ambient->intensity = intensity;
+	ambient->color.r = r / 255.0;
+	ambient->color.g = g / 255.0;
+	ambient->color.b = b / 255.0;
 }
