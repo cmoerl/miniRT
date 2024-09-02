d0fc422c0dff4be7f242707a6a82f94a569d1e3b
MarSonyTM <56359142+MarSonyTM@users.noreply.github.com>
2024-09-02 17:10:51 +0200

    added prints in main to check for issues

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: marianfurnica <marianfurnica@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 16:56:02 by csturm            #+#    #+#             */
-/*   Updated: 2024/09/02 16:33:33 by marianfurni      ###   ########.fr       */
+/*   Updated: 2024/09/02 17:10:01 by marianfurni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,18 @@ int	main(int argc, char **argv)
 		return (1);
 	}
     
+    // Print parsed ambient lighting for testing
+    printf("Ambient lighting intensity: %f\n", scene.amblight.intensity);
+    printf("Ambient lighting color: %f, %f, %f\n", scene.amblight.color.r, scene.amblight.color.g, scene.amblight.color.b);
+
+    // Print parsed camera for testing
+    printf("Camera position: %f, %f, %f\n", scene.camera.center.x, scene.camera.center.y, scene.camera.center.z);
+    printf("Camera orientation: %f, %f, %f\n", scene.camera.orientation.x, scene.camera.orientation.y, scene.camera.orientation.z);
+    printf("Camera FOV: %f\n", scene.camera.fov);
+
+    // Print parsed light for testing
+    printf("Light position: %f, %f, %f\n", scene.light.position.x, scene.light.position.y, scene.light.position.z);
+    printf("Light intensity: %f\n", scene.light.intensity);
 
     // Print parsed planes for testing
     t_plane *plane = scene.objects->planes;
